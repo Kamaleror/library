f4be45eef7cba904819b0518180a3459609d1ebf
Kamaleror <kemo66844@gmail.com>
2025-01-21 07:53:20 +0200

    Add files via upload

---
+++ JS/preloader.js
@@ -0,0 +1,19 @@
+(function ($) {
+  "use strict";
+  var cites = [];
+  cites[0] = "";
+  cites[1] = "";
+  cites[2] = "";
+  cites[3] = "";
+  var cite = cites[Math.floor(Math.random() * cites.length)];
+  $("#preloader p").text(cite);
+  $("#preloader").addClass("loading");
+
+  $(window).on("load", function () {
+    setTimeout(function () {
+      $("#preloader").fadeOut(500, function () {
+        $("#preloader").removeClass("loading");
+      });
+    }, 500);
+  });
+})(jQuery);
